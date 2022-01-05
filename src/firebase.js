15211c852f741a9942b780c35465262f16b751d0
Alex Wower <woweralexander@gmail.com>
2022-01-05 23:03:13 +0000

    firebase file added

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyARCJVhJjI5e_K1YfkDzUb6A7UUguDWsaM",
+  authDomain: "challenge-aed03.firebaseapp.com",
+  projectId: "challenge-aed03",
+  storageBucket: "challenge-aed03.appspot.com",
+  messagingSenderId: "338821285034",
+  appId: "1:338821285034:web:57d2cd15dca3c367fa3cd3",
+  measurementId: "G-ERS8ZT16GQ",
+};
