60e7535bf4cc2c08daeb90011b4db5d918b2a587
Alex Wower <woweralexander@gmail.com>
2022-01-06 16:22:31 +0000

    nav styled, no functionality

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 
 const Header = () => {
   return (
@@ -20,13 +21,20 @@ const Header = () => {
           <span className="header__optionLineOne">Hello Gueast</span>
           <span className="header__optionLineTwo">Sign In</span>
         </div>
+        
         <div className="header__option">
           <span className="header__optionLineOne">Returns</span>
           <span className="header__optionLineTwo">& Orders</span>
         </div>
+
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
+        </div>
+
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
       </div>
     </div>
