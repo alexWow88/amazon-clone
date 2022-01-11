4205b2a153f374e674d66797ccbebfbc93829c66
Alex Wower <woweralexander@gmail.com>
2022-01-11 23:47:05 +0000

    product component created

---
+++ src/components/Product/Product.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Product = () => {
+  return (
+    <div className="product">
+      <p>The lean startup</p>
+      <p className="product__price">
+        <small>$</small>
+        <strong>19.99</strong>
+      </p>
+    </div>
+  );
+};
+
+export default Product;
