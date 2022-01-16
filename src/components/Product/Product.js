440ec6bc606cb2babb1172db56df2351d00542fd
Alex Wower <woweralexander@gmail.com>
2022-01-16 14:12:18 +0000

    props added to Product class and rendering

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,19 +1,21 @@
 import React from "react";
-import './Product.css';
+import "./Product.css";
 
-const Product = () => {
+const Product = ({ title, image, price, rating }) => {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The lean startup</p>
+        <p>{title}</p>
         <p className="product__price">
           <small>$</small>
-          <strong>19.99</strong>
+          <strong>{price}</strong>
         </p>
         <div className="product__rating">
-          <p>⭐</p>
-          <p>⭐</p>
-          <p>⭐</p>
+          {Array(rating)
+            .fill()
+            .map((_) => (
+              <p>⭐</p>
+            ))}
         </div>
       </div>
       <img
