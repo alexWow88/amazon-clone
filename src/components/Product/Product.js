8f97a2b3eeb3552e1abae12f65e27ff592a7436b
Alex Wower <woweralexander@gmail.com>
2022-01-16 13:32:24 +0000

    rating stars added and first product image

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,11 +3,22 @@ import React from "react";
 const Product = () => {
   return (
     <div className="product">
-      <p>The lean startup</p>
-      <p className="product__price">
-        <small>$</small>
-        <strong>19.99</strong>
-      </p>
+      <div className="product__info">
+        <p>The lean startup</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>19.99</strong>
+        </p>
+        <div className="product__rating">
+          <p>⭐</p>
+          <p>⭐</p>
+          <p>⭐</p>
+        </div>
+      </div>
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+        alt=""
+      />
     </div>
   );
 };
