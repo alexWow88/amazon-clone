1fd201596836d7727bb16a834433381c7260312f
Alex Wower <woweralexander@gmail.com>
2022-01-17 14:40:34 +0000

    amazon logo is now a link to home

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -4,7 +4,18 @@ import "./Checkout.css";
 const Checkout = () => {
   return (
     <div className="checkout">
-      <h1>Smash the like</h1>
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt=""
+        />
+
+        <div>
+          <h2 className="checkout__title">Your Shopping Basket</h2>
+        </div>
+      </div>
+      <div className="checkout__right"></div>
     </div>
   );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,14 +2,17 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header">
-      <img
-        className="header__logo"
-        src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
-      />
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        />
+      </Link>
 
       <div className="header__search">
         <input className="header__searchInput" type="text" />
@@ -21,7 +24,7 @@ const Header = () => {
           <span className="header__optionLineOne">Hello Gueast</span>
           <span className="header__optionLineTwo">Sign In</span>
         </div>
-        
+
         <div className="header__option">
           <span className="header__optionLineOne">Returns</span>
           <span className="header__optionLineTwo">& Orders</span>
