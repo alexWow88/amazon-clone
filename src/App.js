8689bf6dd2bec337f6c29323ec9ae10c54b04d3b
Alex Wower <woweralexander@gmail.com>
2022-01-17 10:44:56 +0000

    react router added with checkout routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/checkout" element={<h1>I AM a CHECKOUT</h1>} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
