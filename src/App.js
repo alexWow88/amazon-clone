6e0cf9b01791a51bba3ae15d3339b583f85b284c
Alex Wower <woweralexander@gmail.com>
2022-01-18 16:01:08 +0000

    Login component created and linked with router

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Checkout from "./components/Checkout/Checkout";
+import Login from "./components/Login/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <Router>
       <div className="app">
-        <Header />
         <Routes>
-          <Route path="/login" element={<h1>login</h1>} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/checkout" element={<><Header /><Checkout /></>} />
+          <Route path="/" element={<><Header /><Home /></>} />
         </Routes>
       </div>
     </Router>
