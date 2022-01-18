28c4d177cb80feec7cc2f9d38cb6a06cbec52c80
Alex Wower <woweralexander@gmail.com>
2022-01-18 15:44:28 +0000

    remove items feature added

--- src/components/CheckoutProduct/CheckoutProduct.js
+++ src/components/CheckoutProduct/CheckoutProduct.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { useStateValue } from "../../StateProvider";
 import "./CheckoutProduct.css";
 
 const CheckoutProduct = ({ id, image, title, price, rating }) => {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const removeFromBasket = () => {
+    // remove from basket
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: id,
+    });
+  };
+
   return (
     <div className="checkoutProduct">
       <img className="checkoutProduct__image" src={image} />
@@ -19,7 +30,7 @@ const CheckoutProduct = ({ id, image, title, price, rating }) => {
               <p>⭐</p>
             ))}
         </div>
-        <button>Remove from Basket</button>
+        <button onClick={removeFromBasket}>Remove from Basket</button>
       </div>
     </div>
   );
