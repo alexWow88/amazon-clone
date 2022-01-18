8a7192c6ca9e5bc14ff38282487057f5b9eb93f2
Alex Wower <woweralexander@gmail.com>
2022-01-18 14:23:15 +0000

    subtotal component updating count and price

--- src/components/Subtotal/Subtotal.js
+++ src/components/Subtotal/Subtotal.js
@@ -2,15 +2,23 @@ import React from "react";
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 import { SportsBasketball } from "@mui/icons-material";
+import { useStateValue } from "../../StateProvider";
 
 const Subtotal = () => {
+  const [{ basket }, dispatch] = useStateValue();
+  let subtotal = 0;
+
+  for (let i = 0; i < basket.length; i++) {
+    subtotal += basket[i].price;
+  }
+
   return (
     <div className="subtotal">
       <CurrencyFormat
         renderText={(value) => (
           <>
             <p>
-              Subtotal (0 items) :<strong>0</strong>
+              Subtotal ({basket.length} items) :<strong> ${subtotal}</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" /> This order contains a gift
