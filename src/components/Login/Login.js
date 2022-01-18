2a5ab951e0788a3f37db7287f23bbd13292ef9a6
Alex Wower <woweralexander@gmail.com>
2022-01-18 23:21:54 +0000

    signin and register functions started

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault();
+    //some fancy firebase login code
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+    //do some fancy firebase register code
+  };
+
   return (
     <div className="login">
       <Link to="/">
@@ -15,12 +28,26 @@ const Login = () => {
         <h1>Sign-in</h1>
         <form>
           <h5>E-mail</h5>
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
           <h5>Password</h5>
-          <input type="password" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-          <button className="login__signInButton">Sign In</button>
+          <button
+            className="login__signInButton"
+            type="submit"
+            onClick={signIn}
+          >
+            Sign In
+          </button>
         </form>
 
         <p>
@@ -28,7 +55,7 @@ const Login = () => {
           Please see our Privacy Notice, our Cookies Notice and our
           Interest-Based Ads Notice.
         </p>
-        <button className="login__registerButton">
+        <button className="login__registerButton" onClick={register}>
           Create your Amazon Account
         </button>
       </div>
