197b60ffec96ebc5158c34c0d11bedc4670e874c
Alex Wower <woweralexander@gmail.com>
2022-01-18 14:37:15 +0000

    subtotal value added using reduce

--- src/components/Subtotal/Subtotal.js
+++ src/components/Subtotal/Subtotal.js
@@ -3,14 +3,10 @@ import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 import { SportsBasketball } from "@mui/icons-material";
 import { useStateValue } from "../../StateProvider";
+import { getBasketTotal } from "../../reducer";
 
 const Subtotal = () => {
   const [{ basket }, dispatch] = useStateValue();
-  let subtotal = 0;
-
-  for (let i = 0; i < basket.length; i++) {
-    subtotal += basket[i].price;
-  }
 
   return (
     <div className="subtotal">
@@ -18,7 +14,7 @@ const Subtotal = () => {
         renderText={(value) => (
           <>
             <p>
-              Subtotal ({basket.length} items) :<strong> ${subtotal}</strong>
+              Subtotal ({basket.length} items) :<strong> {value}</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" /> This order contains a gift
@@ -26,7 +22,7 @@ const Subtotal = () => {
           </>
         )}
         decimalScale={2}
-        value={0}
+        value={getBasketTotal(basket)}
         displayType={"text"}
         thousandSeparator={true}
         prefix={"$"}
